6bf00acb146847380823df096c5bb2777098a9b3
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2021-07-16 18:47:15 +0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+function ordinal_suffix_of(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+function calc_date() {
+	// test it
+	const a = new Date("2020-03-01");
+	const b = new Date();
+	const difference = dateDiffInDays(a, b);
+	str = ordinal_suffix_of(difference) + " of March, 2020";
+	return str
+}
+function date() {
+	document.getElementById("date").innerText = calc_date();
+}
